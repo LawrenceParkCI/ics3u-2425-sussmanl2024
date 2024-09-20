3dd147a4db0f941e4ecff8cd1d374772cdf2aeb0
349176552 <349176552@C-MXL23835BS.tdsb.on.ca>
2024-09-20 11:10:54 -0400

    Adding ICS3U project with helloworld class

---
+++ ICS3U - SussmanL/src/unit1/HelloWorld.java
@@ -0,0 +1,22 @@
+package unit1;
+/**
+ * Description: This program prints Hello World and a second statement 
+ * Date: 19/9/2024 
+ * @author Leo Sussman
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		//prints Hello World to the system console
+		System.out.println("Hello World!");
+		System.out.println("17");
+		
+		/*
+		 * This is a block comment
+		 * you can add notes on multiple lines
+		 */
+		
+		
+	}
+
+}
