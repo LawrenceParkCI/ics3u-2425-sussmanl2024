fe89f12c52ccb5a544afb74a0d6dd966da260272
349176552 <349176552@C-MXL23835BS.tdsb.on.ca>
2024-09-20 11:32:42 -0400

    ICS3U - Made AboutMe Class, made it print about my name, grade, school and teacher.

---
+++ ICS3U - SussmanL/src/unit1/AboutMe.java
@@ -0,0 +1,17 @@
+package unit1;
+/**
+ * Description: This program prints some information about me, the author <br>
+ * Date: 20/9/2024 
+ * @author Leo Sussman
+ */
+public class AboutMe {
+
+	public static void main(String[] args) {
+		//this prints the text
+		System.out.println("Here I will tell you a little about me");
+		System.out.println("My name is Leo,");
+		System.out.println("I am in the 11th grade at Lawence Park");
+		System.out.println("And my teacher for computer science is Ms. Kemp");
+	}
+
+}
