b38ca98219af1ad15ff71c565bcfbe1d6d2956db
349176552 <349176552@C-MXL23835BS.tdsb.on.ca>
2024-09-25 10:54:44 -0400

    Started to randomly generate numbers

---
+++ ICS3U - SussmanL/src/unit1/Bingo.java
@@ -0,0 +1,13 @@
+package unit1;
+public class Bingo {
+	//saving the % symbol, as my computer can not type it
+	public static void main(String[] args) {
+	System.out.println("Bingo Card:");
+		int cBMin =1;
+		int cBMax = 15;
+		
+		int random_int = (int)Math.floor(Math.random() * (cBmin - cBMax + 1) +cBmin);
+		
+	}
+
+}
