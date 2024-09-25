02dbfd95b50ed479f39672fae37fe7de60708e7f
349176552 <349176552@C-MXL23835BS.tdsb.on.ca>
2024-09-25 10:29:19 -0400

    Added the timetable for the week of September 23rd

--- ICS3U - SussmanL/src/unit1/AboutMe.java
+++ ICS3U - SussmanL/src/unit1/AboutMe.java
@@ -8,10 +8,44 @@ public class AboutMe {
 
 	public static void main(String[] args) {
 		//this prints the text
-		System.out.println("Here I will tell you a little about me");
+		System.out.println("Here I will  tell you a little about me");
 		System.out.println("My name is Leo,");
 		System.out.println("I am in the 11th grade at Lawence Park");
 		System.out.println("And my teacher for computer science is Ms. Kemp");
+		System.out.println("GO PANTHERS!!!");
+		System.out.println("////////////////////////////////////////////////////////////");
+		System.out.println("Timetable for the week of September 23rd");
+		System.out.println("_________________________________________________________");
+		System.out.println("Monday:");
+		System.out.format("Class: %-15s|Time: %4s", "Computer Science","9:00-10:15 \n");
+		System.out.format("Class: %-16s|Time: %4s", "English","10:20-11:40 \n");
+		System.out.format("Class: %-16s|Time: %4s", "Biology","12:40-1:55 \n");
+		System.out.format("Class: %-16s|Time: %4s", "Spare","2:00-3:15 \n");
+		System.out.println("_________________________________________________________");
+		System.out.println("Tuesday:");
+		System.out.format("Class: %-16s|Time: %4s", "English","9:00-10:15 \n");
+		System.out.format("Class: %-15s|Time: %4s", "Computer Science","10:20-11:40 \n");
+		System.out.format("Class: %-16s|Time: %4s", "Spare","12:40-1:55 \n");
+		System.out.format("Class: %-16s|Time: %4s", "Biology","2:00-3:15 \n");
+		System.out.println("_________________________________________________________");
+		System.out.println("Wednesday:");
+		System.out.format("Class: %-15s|Time: %4s", "Computer Science","9:55-10:55 \n");
+		System.out.format("Class: %-16s|Time: %4s", "English","11:00-12:00 \n");
+		System.out.format("Class: %-16s|Time: %4s", "Biology","1:05-2:15 \n");
+		System.out.format("Class: %-16s|Time: %4s", "Spare","2:10-3:15 \n");
+		System.out.println("_________________________________________________________");
+		System.out.println("Thursday:");
+		System.out.format("Class: %-16s|Time: %4s", "English","9:00-10:15 \n");
+		System.out.format("Class: %-15s|Time: %4s", "Computer Science","10:20-11:40 \n");
+		System.out.format("Class: %-16s|Time: %4s", "Spare","12:40-1:55 \n");
+		System.out.format("Class: %-16s|Time: %4s", "Biology","2:00-3:15 \n");
+		System.out.println("_________________________________________________________");
+		System.out.println("Friday:");
+		System.out.format("Class: %-15s|Time: %4s", "Computer Science","9:00-10:15 \n");
+		System.out.format("Class: %-16s|Time: %4s", "English","10:20-11:40 \n");
+		System.out.format("Class: %-16s|Time: %4s", "Biology","12:40-1:55 \n");
+		System.out.format("Class: %-16s|Time: %4s", "Spare","2:00-3:15 \n");
+		System.out.println("_________________________________________________________");
 	}
 
 }
