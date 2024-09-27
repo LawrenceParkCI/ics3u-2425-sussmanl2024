c337c43ce0659c0c2c1e97529e2642dda50e3d91
349176552 <349176552@C-MXL23835BS.tdsb.on.ca>
2024-09-27 10:06:09 -0400

    formatted it

--- ICS3U - SussmanL/src/unit1/Bingo.java
+++ ICS3U - SussmanL/src/unit1/Bingo.java
@@ -5,7 +5,7 @@ package unit1;
  * @author Leo Sussman
  */
 public class Bingo {
-	
+
 	public static void main(String[] args) {
 		//rules of bingo
 		System.out.println("Bingo Card:");
@@ -17,7 +17,7 @@ public class Bingo {
 		System.out.println("5. Steps 1 to 4 are repeated until a player matches the BINGO pattern");
 		System.out.println("6. The winning player yells BINGO!");
 		System.out.println("_____________________________________________________________________");
-// prints the squares to play on
+		// prints the squares to play on
 		System.out.format(" B %5s %5s %5s %5s ","I", "N", "G", "O");
 		System.out.format("\n 2 %5s %5s %5s %5s ","16", "32", "48", "62");
 		System.out.format("\n 7 %5s %5s %5s %5s ","21", "34", "54", "64");
