0f13b98847692c0970392e7fa2188b876df5725f
349176552 <349176552@C-MXL23835BS.tdsb.on.ca>
2024-09-27 10:09:50 -0400

    Made ascii art of my name, LEO

--- ICS3U - SussmanL/src/unit1/AsciiTextandImage.java
+++ ICS3U - SussmanL/src/unit1/AsciiTextandImage.java
@@ -1,5 +1,37 @@
 package unit1;
 
 public class AsciiTextandImage {
-
+	public static void main(String[] args) {
+System.out.println("LLLLLLLLLLL             EEEEEEEEEEEEEEEEEEEEEE     OOOOOOOOO");
+System.out.println("L:::::::::L             E::::::::::::::::::::E   OO:::::::::OO   \r\n"
+		+ "");
+System.out.println("L:::::::::L             E::::::::::::::::::::E OO:::::::::::::OO \r\n"
+		+ "");
+System.out.println("LL:::::::LL             EE::::::EEEEEEEEE::::EO:::::::OOO:::::::O\r\n"
+		+ "");
+System.out.println("  L:::::L                 E:::::E       EEEEEEO::::::O   O::::::O\r\n"
+		+ "");
+System.out.println("  L:::::L                 E:::::E             O:::::O     O:::::O\r\n"
+		+ "");
+System.out.println("  L:::::L                 E::::::EEEEEEEEEE   O:::::O     O:::::O\r\n"
+		+ "");
+System.out.println("  L:::::L                 E:::::::::::::::E   O:::::O     O:::::O\r\n"
+		+ "");
+System.out.println("  L:::::L                 E:::::::::::::::E   O:::::O     O:::::O\r\n"
+		+ "");
+System.out.println("  L:::::L                 E::::::EEEEEEEEEE   O:::::O     O:::::O\r\n"
+		+ "");
+System.out.println("  L:::::L                 E:::::E             O:::::O     O:::::O\r\n"
+		+ "");
+System.out.println("  L:::::L         LLLLLL  E:::::E       EEEEEEO::::::O   O::::::O\r\n"
+		+ "");
+System.out.println("LL:::::::LLLLLLLLL:::::LEE::::::EEEEEEEE:::::EO:::::::OOO:::::::O\r\n"
+		+ "");
+System.out.println("L::::::::::::::::::::::LE::::::::::::::::::::E OO:::::::::::::OO \r\n"
+		+ "");
+System.out.println("L::::::::::::::::::::::LE::::::::::::::::::::E   OO:::::::::OO   \r\n"
+		+ "");
+System.out.println("LLLLLLLLLLLLLLLLLLLLLLLLEEEEEEEEEEEEEEEEEEEEEE     OOOOOOOOO     \r\n"
+		+ "");
+	}
 }
