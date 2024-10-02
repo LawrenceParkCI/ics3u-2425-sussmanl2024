dcaca31b3e6a698e20733722cb7a4df07d197a9e
349176552 <349176552@C-MXL23835BS.tdsb.on.ca>
2024-10-02 11:22:34 -0400

    Made a program asking for any 2 numbers, that then reads them out to
    you after

---
+++ ICS3U - SussmanL/src/unit1/InputFP.java
@@ -0,0 +1,23 @@
+package unit1;
+
+import java.util.Scanner;
+public class InputFP {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		double num1, num2;
+		System.out.println("AREA PROGRAM");
+		System.out.println("Type in a number and press <ENTER>");
+		num1 = sc.nextDouble();
+		
+		System.out.println("Type in a number and press <ENTER>");
+		num2 = sc.nextDouble();
+		
+		System.out.println();
+		System.out.println("The first number entered was " + num1);
+		System.out.println("The second number entered was " + num2);
+		sc.close();
+	}
+
+}
