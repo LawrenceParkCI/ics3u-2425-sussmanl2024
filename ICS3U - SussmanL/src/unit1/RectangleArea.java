796606d2faac05a68bbeca3c76b3fa1bcab57b69
349176552 <349176552@C-MXL23835BS.tdsb.on.ca>
2024-10-02 11:10:57 -0400

    Made a program that asks for the side lengths of a prism and gets the area for you

---
+++ ICS3U - SussmanL/src/unit1/RectangleArea.java
@@ -0,0 +1,25 @@
+package unit1;
+import java.util.Scanner;
+public class RectangleArea {
+
+	public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	int length, width, depth;
+	
+	System.out.println("AREA PROGRAM");
+	System.out.println("Type in the length of the prism and <ENTER>");
+	length = sc.nextInt();
+	
+	System.out.println("Type in the width of the prism and <ENTER>");
+	width = sc.nextInt();
+	
+	System.out.println("Type in the depth of the prism and <ENTER>");
+	depth = sc.nextInt();
+	
+	int area = length * width * depth;
+	
+	System.out.println("The area of your prism is " + area);
+	sc.close();
+	}
+
+}
