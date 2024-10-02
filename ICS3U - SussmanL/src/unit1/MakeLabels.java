3264b997f15324634d3a04d94814b8923f25823c
349176552 <349176552@C-MXL23835BS.tdsb.on.ca>
2024-10-02 10:59:06 -0400

    Made a scanner to ask for your name and subject

---
+++ ICS3U - SussmanL/src/unit1/MakeLabels.java
@@ -0,0 +1,22 @@
+package unit1;
+import java.util.Scanner;
+
+public class MakeLabels {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		String subject;
+		String name;
+		System.out.println("Type in your name and press <ENTER>");
+		name=sc.nextLine();
+		System.out.println("Type in the subject and press <ENTER>");
+		subject=sc.nextLine();
+		System.out.println();
+		System.out.println("********************************");
+		System.out.println(name);
+		System.out.println(subject);
+		System.out.println("********************************");
+		sc.close();
+	}
+
+}
