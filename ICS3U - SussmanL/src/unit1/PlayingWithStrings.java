1eb7fffa54b7569377baa96a7cfa449127d06060
349176552 <349176552@C-MXL23835BS.tdsb.on.ca>
2024-10-03 10:13:10 -0400

    Started to use strings and positions

---
+++ ICS3U - SussmanL/src/unit1/PlayingWithStrings.java
@@ -0,0 +1,19 @@
+package unit1;
+/**
+ * Description: This program teaches me how to create variables<br>
+ * Date: 3/10/2024 
+ * @author Leo Sussman
+ */
+import java.util.Scanner;
+public class PlayingWithStrings {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		
+		
+		
+		sc.close();
+	}
+
+}
