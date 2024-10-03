b91d0312c72a065551e3b7dd7a2a9ce16724df73
349176552 <349176552@C-MXL23835BS.tdsb.on.ca>
2024-10-03 10:12:47 -0400

    Learned how to use chars

---
+++ ICS3U - SussmanL/src/unit1/InputChars.java
@@ -0,0 +1,24 @@
+package unit1;
+/**
+ * Description: This program teaches me how to create variables<br>
+ * Date: 3/10/2024 
+ * @author Leo Sussman
+ */
+import java.util.Scanner;
+public class InputChars {
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		char ch1, ch2, ch3;
+		System.out.println("Type in any three characters on the keyboard");
+		System.out.println("Press <ENTER> after each one");
+		ch1 = sc.nextLine().charAt(0);
+		ch2 = sc.nextLine().charAt(0);
+		ch3 = sc.nextLine().charAt(0);
+		System.out.println();
+		System.out.print("Together these 3 letters spell: " + (ch1 + ch2 + ch3));
+		sc.close();
+	}
+
+}
