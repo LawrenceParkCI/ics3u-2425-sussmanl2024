c052df650514b19ab4e13f450c8b9c393cc8a83a
349176552 <349176552@C-MXL23835BS.tdsb.on.ca>
2024-10-04 11:40:14 -0400

    Started doing the assignments

---
+++ ICS3U - SussmanL/src/unit1/Alive.java
@@ -0,0 +1,49 @@
+package unit1;
+/**
+ * Description: This program calculates how many hours I've been asleep in my life<br>
+ * Date: 4/10/2024 
+ * @author Leo Sussman
+ */
+import java.util.Scanner;
+public class Alive {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+
+		
+		
+		int birthYear;
+		int birthMonth;
+		int birthDay;
+		int currentYear;
+		int currentMonth;
+		int currentDay;
+		double hoursDaily;
+		System.out.println("After each question, answer in number form and then press <ENTER>");
+		System.out.println("What year were you born?");
+		birthYear=sc.nextInt();
+		System.out.println("What month were you born?");
+		birthMonth=sc.nextInt();
+		System.out.println("What day were you born?");
+		birthDay=sc.nextInt();
+		System.out.println("What year is it?");
+		currentYear=sc.nextInt();
+		System.out.println("What month is it?");
+		currentMonth=sc.nextInt();
+		System.out.println("What day is it?");
+		currentDay=sc.nextInt();
+		System.out.println("On average how many hours do you sleep per day");
+		hoursDaily=sc.nextDouble();
+
+		
+		double daysAlive = 365 * (currentYear-birthYear) + 30 * (currentMonth-birthMonth) + (currentDay-birthDay);
+		double hoursSlept = daysAlive * hoursDaily;
+		double yearsAlive = daysAlive/365;
+	
+		System.out.println("You have been alive for "  + yearsAlive + " years");
+		System.out.println("You have been alive for "  + daysAlive + " days");
+		System.out.println("You have slept for " + hoursSlept + " hours");
+		sc.close();
+	}
+
+}
---
+++ ICS3U - SussmanL/src/unit1/NetPay.java
@@ -0,0 +1,19 @@
+package unit1;
+/**
+ * Description: This program finds the net pay for an employee<br>
+ * Date: 4/10/2024 
+ * @author Leo Sussman
+ */
+public class NetPay {
+
+	public static void main(String[] args) {
+		double hours = 40.00;
+		double wage = 5.00;
+		double insurance = 2.00;
+		double tax = 0.78;
+
+		double netPay = (hours * wage - insurance) * tax;
+		System.out.println("The net pay for the employee is $" + netPay);
+	}
+
+}
