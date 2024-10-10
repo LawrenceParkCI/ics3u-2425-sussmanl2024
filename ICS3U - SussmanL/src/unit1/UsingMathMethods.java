74bac832df72631a4779315d845852f935ae16a8
349176552 <349176552@C-MXL23835BS.tdsb.on.ca>
2024-10-10 10:56:32 -0400

    Finished the 2 worksheets from october 9th

--- ICS3U - SussmanL/src/unit1/UsingMathMethods.java
+++ ICS3U - SussmanL/src/unit1/UsingMathMethods.java
@@ -1,20 +1,15 @@
 package unit1;
 /**
  * Description: This program teaches me how to use math methods<br>
- * Date: 4/10/2024 
+ * Date: 10/10/2024 
  * @author Leo Sussman
  */
 import java.util.Scanner;
 public class UsingMathMethods {
-
-	/**
-	 * 
-	 * @param args
-	 */
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		double num;
-		
+
 		System.out.println("Part A");
 		//Write code to ask the user for any real number to be input.
 		//Store their input into the num variable
@@ -27,7 +22,7 @@ public class UsingMathMethods {
 		System.out.println(Math.pow(num, 2));
 		System.out.println(Math.pow(num, num));
 		System.out.println(Math.pow(num, 3));
-		
+
 		/*
 		 * Run the program and write down the output that is produced
 		 * when each of the following values are input:
@@ -43,25 +38,25 @@ public class UsingMathMethods {
 		 * Math.pow(num, 3)	4699.421875		|	64.0		|-12895.213624999999
 		 * 
 		 */
-		
+
 		//Why does the last value(-23.45) give strange output for Math.sqrt(num)?
 		//Nan stands for Not a number!
-		
+
 		//You can't square root a negative
-		
+
 		//In your own words describe what the following Math methods does
 		/*
-		 * round:
-		 * rint:
-		 * sqrt:
-		 * abs:
-		 * pow:
+		 * round: rounds to the nearest integer
+		 * rint: rounds to nearest real number
+		 * sqrt: gets the square root
+		 * abs: gets the absolute number
+		 * pow: put a number to an exponent
 		 */
-		
+
 		//The methods round, sqrt and abs only have one parameter (which is in the brackets).
 		//Why does the pow method have 2 parameters? Explain the purpose of these 2 parameters.
-		
-		
+		//the first number is the base and the next number is the exponent
+
 		System.out.println("Part B");
 		//Ask the user for any real number and an integer to be input. Display the real number,
 		//and its square root. Also determine and display the real number raised to the integer power.
@@ -70,27 +65,58 @@ public class UsingMathMethods {
 		//Eg. If the real number 13.5 nad the integer 2 are input, display
 		//REAL NUMBER	SQUARE ROOT		RAISED TO POWER 2 <- display power here
 		//   13.5		   3.6742			182.25
-		
-		
-		
-		
+
+		double real;
+		double integer;
+
+		System.out.println("Please type any real number then press <ENTER>");
+		real=sc.nextDouble();
+		System.out.println("Please type any real number then press <ENTER> to change the exponent");
+		integer=sc.nextDouble();
+		System.out.println("____________________________________");
+		System.out.println("REAL NUMBER: " + real);
+		System.out.println("____________________________________");
+		System.out.println("SQUARE ROOT: " + Math.sqrt(real));
+		System.out.println("____________________________________");
+		System.out.println("RAISED TO POWER " + integer + ": " + Math.pow(real, integer));
+		System.out.println("____________________________________");
 		//Write down the results produced by your program when the real number 7.8985 and
 		//the integer 3 are input.
-		
-		
-		
+		//REAL NUMBER: 7.8985
+
+		//SQUARE ROOT: 2.8104270138183627
+
+		//RAISED TO POWER 3: 492.75820832162503
+
+
 		//Change your program so that instead of inputting an integer power, a real number
 		//power can be input.
 		//Write down the results produced by your program when the following are input:
 		//a) the number 16 and the real number power 0.5
 		//b) the number 8 and the real number power 0.33
-		
-		
+
+		/*
+		REAL NUMBER: 16.0
+		____________________________________
+		SQUARE ROOT: 4.0
+		____________________________________
+		RAISED TO POWER 0.5: 4.0
+		____________________________________
+
+
+		REAL NUMBER: 8.0
+		____________________________________
+		SQUARE ROOT: 2.8284271247461903
+		____________________________________
+		RAISED TO POWER 0.33: 1.9861849908740719
+		____________________________________
+
+		 */
 		//Does your program give a different answer if 0.3333333 is input instead of 0.33 for the power?
-		
+		//yes
 		sc.close();
-		
-		
+
+
 	}
 
 }
