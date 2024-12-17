22d161b8b9f36da5a3d171f53386b1850d5f4472
349176552 <349176552@C-MXL23835BS.tdsb.on.ca>
2024-12-17 09:59:56 -0500

    added param comments

--- ICS3U - SussmanL/src/unit3/MathPlus.java
+++ ICS3U - SussmanL/src/unit3/MathPlus.java
@@ -8,16 +8,34 @@ public class MathPlus {
 		numOfFactors(17);
 		isPrime(23);
 	}
-
+/**
+ * gets the distance value for two distances
+ * @param x1 the first x value
+ * @param y1 the first y value
+ * @param x2 the second x value
+ * @param y2 the second y value
+ * @return the distance between x and y
+ */
 public static double distance( int x1, int y1, int x2, int y2 ) {
 	System.out.println("" + (x1-x2) + ", " +(y1-y2));
 	return 0;
 	
 }
+/**
+ * gets the hypotenuse of a rectangle
+ * @param a the first side
+ * @param b the second side
+ * @return the last side length
+ */
 public static double hypotenuse(double a, double b) {
 	System.out.println(Math.sqrt((Math.pow(a, 2) + Math.pow(b, 2))));
 	return 0;
 }
+/**
+ * gets the number of factors that a number has
+ * @param num the number that is being tested
+ * @return the number of factors
+ */
 public static int numOfFactors(int num) {
 	int factors = 0;
 		for (int i = 1; i <= num; i++) {
@@ -30,6 +48,11 @@ public static int numOfFactors(int num) {
 		System.out.println(factors);
 	return 0;
 }
+/**
+ * finds if a number is prime or not
+ * @param num the number that is being tested
+ * @return true if prime false if not
+ */
 public static boolean isPrime(int num) {
 	System.out.println("PRIME:");
 	int numInput = 0;
