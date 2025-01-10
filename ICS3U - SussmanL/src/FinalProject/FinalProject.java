3527f8310c76f1ee5594715a2a27b13d4d81d883
349176552 <349176552@C-MXL23835BS.tdsb.on.ca>
2025-01-10 11:40:42 -0500

    errors with playagain and need to get aces working

--- ICS3U - SussmanL/src/FinalProject/FinalProject.java
+++ ICS3U - SussmanL/src/FinalProject/FinalProject.java
@@ -16,64 +16,89 @@ public class FinalProject {
 	static int pause = 1;
 	public static void main(String[] args) throws InterruptedException {
 		double money = 1000;
+		boolean bust = false;
+		String choice;
 		ArrayList<String> deck;
 		ArrayList<String> dealerHand = new ArrayList<>();
 		ArrayList<String> playerHand = new ArrayList<>();
-		
+
 		boolean playAgain = true;
 		c.println("Welcome to Blackjack!");
 		c.println("In Blackjack, everyone plays against the dealer. \n"
 				+ "Players receive all cards face up and the dealer's first card is face up and the second is face down. \n"
 				+ "The object of the game is to get closer to 21 than the dealer without going over 21. \n"
 				+ "If a hand goes over 21, it is called a “bust” or “break” and the wager is lost.\n");
-		
-		
+
+
 		while (playAgain == true) {
-		boolean betMade = false;
-		deck = makeDeck();//getting a randomized deck
-        Collections.shuffle(deck);
-		printSlow("How much would you like to bet? (whole dollar ammounts please)", pause);
-		printSlow("You currently have $" + money, pause);
-		double bet = c.readDouble();
+			boolean betMade = false;
+			deck = makeDeck();//getting a randomized deck
+			Collections.shuffle(deck);
+			printSlow("How much would you like to bet? (whole dollar ammounts please)", pause);
+			printSlow("You currently have $" + money, pause);
+			double bet = c.readDouble();
+			if (bet<= money) {
+				money = money - bet;
+				betMade = true;
+				printSlow("You bet $" + bet, pause);
+			}else {
+				printSlow("That is not a valid bet", pause);
+			}
 
-        
-        
-        
-        while (betMade == true) {
-		playerHand.add(deck.remove(0));//giving the player 2 cards
-		playerHand.add(deck.remove(0));
-		printSlow("Your hand is: " + playerHand.getFirst() + " and " + playerHand.getLast(), pause);
-		dealerHand.add(deck.remove(0));//giving the dealer two cards but only one is shown
-		dealerHand.add(deck.remove(0));
-		printSlow("The dealer is showing: " + dealerHand.getFirst(), pause);
-		
-		
-		
-		
-		
-        }
-            
-            
-            
-            
-            
-            
-            c.println("Do you want to play again?(Y/N)");
-            String choice = c.readLine();
-            if (choice.equalsIgnoreCase(choice)) {
-            	playAgain = true;
-            }else
-            	playAgain = false;
+			while (betMade == true) { // starting the turn
+				playerHand.add(deck.remove(0));//giving the player 2 cards
+				playerHand.add(deck.remove(0));
+				printSlow("Your hand is: " + playerHand.getFirst() + " and " + playerHand.getLast(), pause);
+				dealerHand.add(deck.remove(0));//giving the dealer two cards but only one is shown
+				dealerHand.add(deck.remove(0));
+				printSlow("The dealer is showing: " + dealerHand.getFirst(), pause);
+				if (handValue(playerHand) == 21) {//auto win if you start with blacjack
+					System.out.println("Blackjack! You win!");
+				}else if (handValue(playerHand) < 21){ //all other times you get to hit or stand
+					printSlow("Do you want to hit or stand", pause);
+					choice = c.readLine();
+					if(choice.equalsIgnoreCase("hit")) {
+						playerHand.add(deck.remove(0));
+						printSlow("You get drawn the " + playerHand.getLast(), pause);
+						while (!choice.equalsIgnoreCase("stand") && bust!=true) {
+							if (handValue(playerHand) <= 21) {
+								printSlow("Do you want to hit or stand", pause);
+								choice = c.readLine();
+								playerHand.add(deck.remove(0));
+								printSlow("You get drawn the " + playerHand.getLast(), pause);
+							}else {
+								printSlow("YOU BUST. ROUND OVER", pause);
+								bust = true;
+							}
+						}
+					}
+				}
+
+				if (bust ==false) {
+					printSlow("You stand on " + handValue(playerHand), pause);
+					dealerTurn(dealerHand,deck);
+					winner(playerHand, dealerHand);
+				}
+
+				c.println("Do you want to play again?(Y/N)");
+				choice = c.readLine();
+				if (choice.equalsIgnoreCase(choice)) {
+					playAgain = true;
+				}else
+					playAgain = false;
+			}
 		}
-		
-		
-		
-		
-		
-        }
+
+	}
 
 
-	
+
+
+
+
+
+
+
 	public static void printSlow (String str, double pause) throws InterruptedException {
 		for (int i = 1; i < str.length() + 1; i++) { 
 			c.print(str.substring(i - 1, i));
@@ -92,25 +117,25 @@ public class FinalProject {
 		}
 		return deck;
 	}
-	 public static int handValue(ArrayList<String> hand) {
-	        int value = 0;
-	        int aces = 0;
-
-	        for (String card : hand) {
-	            String rank = card.split(" ")[0];
-	            if (rank.equals("Jack") || rank.equals("Queen") || rank.equals("King")) {
-	                value += 10;
-	            } else if (rank.equals("Ace")) {
-	                value += 11;
-	                aces++;
-	            } else {
-	                value += Integer.parseInt(rank);
-	            }
-	        }
-			return value;
-}
-	 public static int winner(ArrayList<String> playerHand,ArrayList<String> dealerHand) throws InterruptedException {
-		 int winner;
+	public static int handValue(ArrayList<String> hand) {
+		int value = 0;
+		for (String card : hand) {
+			String rank = card.split(" ")[0];
+			if (rank.equals("Jack") || rank.equals("Queen") || rank.equals("King")) {
+				value += 10;
+			} else if (rank.equals("Ace")) {
+				if (handValue(hand)>21) {
+					value += 1;
+				}else
+					value +=11;
+			} else {
+				value += Integer.parseInt(rank);
+			}
+		}
+		return value;
+	}
+	public static int winner(ArrayList<String> playerHand,ArrayList<String> dealerHand) throws InterruptedException {
+		int winner;
 		if(handValue(playerHand)>handValue(dealerHand) && handValue(playerHand) <= 21) {
 			printSlow("YOU WIN!!", pause);
 			winner = 2;
@@ -121,6 +146,22 @@ public class FinalProject {
 			printSlow("You Lose", pause);
 			winner = 0;
 		}
-		 return winner;
-	 }
+		return winner;
+	}
+	public static int dealerTurn(ArrayList<String> dealerHand, ArrayList<String> deck) throws InterruptedException{
+		printSlow("The dealer shows " + dealerHand.getLast(), pause);
+		while(handValue(dealerHand) < 17) {
+			Thread.sleep(1000);
+			dealerHand.add(deck.remove(0));
+			printSlow("The dealer draws the " + dealerHand.getLast(), pause);
+		}
+		if(handValue(dealerHand) > 21) {
+			printSlow("The dealer busts", pause);
+			printSlow("YOU WIN!", pause);
+		}else {
+			printSlow("The dealer stands on " + handValue(dealerHand), pause);
+		}
+		return handValue(dealerHand);
+		
+	}
 }
